fd2ad487c51558b1eaf7a96c7ec10bc0630fdfea
tiwarihimanshu <himanshut829@gmail.com>
2024-04-05 14:29:05 +0530

    Ui changes

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,31 +1,3 @@
-// import { useState } from "react";
-// import "./Accordion.css";
-// import AccordionItem from "./AccordionItem";
-
-// const Accordion = ({ accordionData, originalData,updquery,updateTable , setHasUnsavedChanges}) => {
-//   const [activeIndex, setActiveIndex] = useState(0);
-
-//   const handleItemClick = (index) => {
-//     setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
-//   };
-
-//   return (
-//     <div className="container">
-//       {accordionData.map((item, index) => (
-//         <AccordionItem
-//           key={index}
-//           header={item.tableName}
-//           data={item}
-//           updateTable={updateTable}
-//           isOpen={activeIndex === index}
-//           onClick={() => handleItemClick(index)}
-//           setHasUnsavedChanges={ setHasUnsavedChanges}/>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Accordion;
 import { useContext, useState } from "react";
 import "./Accordion.css";
 import AccordionItem from "./AccordionItem"; 
