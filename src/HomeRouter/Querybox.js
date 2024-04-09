8ac46fefb474cc103bd73a1c1043fdaadb942ced
Amit <contact.tripathi@gmail.com>
2024-04-09 13:16:21 +0000

    UI cosmetic chnages

--- src/HomeRouter/Querybox.js
+++ src/HomeRouter/Querybox.js
@@ -1,56 +1,66 @@
-import React, { useContext } from 'react';
-import copy from "../images/copy.jpg";
-import outp from "../images/output.jpg";
-import { AppContext } from '../Context/AppContext';
-import DropDownBox from '../components/DropDownBox';
+import React, { useContext } from "react";
+import { AppContext } from "../Context/AppContext";
 import { MdOutlineOutput } from "react-icons/md";
-import { FaRegCopy } from "react-icons/fa6";
-import { useState } from 'react';
-export default function Querybox({ handleOutpClick,sqldata }){
-
- const [dtquery,setDtquery]=useState(`SELECT CLCL_ID, SBSB_CK,
+import { FaCopy } from "react-icons/fa";
+import { useState } from "react";
+export default function Querybox({ handleOutpClick, sqldata }) {
+  const [dtquery, setDtquery] = useState(`SELECT CLCL_ID, SBSB_CK,
   SUM(CLCL_ID) AS Sumdata,
   MAX(SBSB_CK) AS Maxdata
   FROM claimheader
   WHERE SBSB_CK > 100`);
-  const[outputstatus,setOutputstatus]=useState(false);
-  const { tabs, tableData, updquery, updateTable,dispatch, FetchData,hasUnsavedChanges,Dropdownchangesstatus} = useContext(AppContext);
-    console.log(sqldata,"sqldatassssss")
-     const handleImageClick = () => {
-       setOutputstatus(true)
-      handleOutpClick();
-      
+  const [outputstatus, setOutputstatus] = useState(false);
+  const {
+    tabs,
+    tableData,
+    updquery,
+    updateTable,
+    dispatch,
+    FetchData,
+    hasUnsavedChanges,
+    Dropdownchangesstatus,
+  } = useContext(AppContext);
+
+  const handleImageClick = () => {
+    setOutputstatus(true);
+    handleOutpClick();
   };
- 
-  const iconstyle={
+
+  const iconstyle = {
     height: "30px",
-    width: "30px"
-  }
+    width: "30px",
+  };
 
-    return (
-        <>
-              <div className="output-box">
-              <div className="image-container">
-              <FaRegCopy style={iconstyle} title='Copy'/>
+  return (
+    <>
+      <div className="output-box">
+        <div className="image-container">
+          <FaCopy style={iconstyle} title="Copy" />
 
-                <MdOutlineOutput style={iconstyle} title='output' onClick={handleImageClick}/>
-              </div>
-              <div className="content">
-              {sqldata && outputstatus&&( 
-                <p>{dtquery}</p>)}
-              </div>
-            </div>
-            <div className="output-box">
-              <div className="image-container">
-              <FaRegCopy style={iconstyle} title='Copy'/>
+          <MdOutlineOutput
+            style={iconstyle}
+            title="output"
+            onClick={handleImageClick}
+          />
+        </div>
+        <div className="content">
+          {sqldata && outputstatus && <p>{dtquery}</p>}
+        </div>
+      </div>
+      <div className="output-box">
+        <div className="image-container">
+          <FaCopy style={iconstyle} title="Copy" />
 
-              <MdOutlineOutput style={iconstyle} title='output' onClick={handleImageClick}/>
-              </div>
-              <div className="content">
-              {sqldata && outputstatus&&( 
-                <p>{dtquery}</p>)}
-              </div>
-            </div>
-        </>
-    )
+          <MdOutlineOutput
+            style={iconstyle}
+            title="output"
+            onClick={handleImageClick}
+          />
+        </div>
+        <div className="content">
+          {sqldata && outputstatus && <p>{dtquery}</p>}
+        </div>
+      </div>
+    </>
+  );
 }
